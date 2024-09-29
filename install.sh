9c800dae4f5c2a49a5ad7050fedc23df5f4545c4
Tobirou-Senju <a9998964@gmail.com>
2024-09-29 12:45:57 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,15 +22,6 @@ clear
 apt update
 apt upgrade -y
 
-# Install necessary packages for Brave Browser
-sudo apt install curl -y
-
-# Install Brave Browser
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update
-sudo apt install brave-browser -y
-
 # Making .config and Moving config files and background to Pictures
 cd $builddir
 mkdir -p /home/$username/.config
