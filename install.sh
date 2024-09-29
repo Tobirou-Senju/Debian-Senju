c8b9b2d1d1f7774f0e08aae22e171b57f4d790cd
Tobirou-Senju <a9998964@gmail.com>
2024-09-29 12:30:41 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+# Check if Script is Run as Root
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
+  exit 1
+fi
+
+username=$(id -u -n 1000)
+builddir=$(pwd)
+
+# Install Terminus Fonts
+sudo apt install fonts-terminus
+
+# Set the font to Terminus Fonts
+setfont /usr/share/consolefonts/Uni3-TerminusBold28x14.psf.gz
+
+# Clear the screen
+clear
+
+# Update packages list and update system
+apt update
+apt upgrade -y
+
+# Install necessary packages for Brave Browser
+sudo apt install curl -y
+
+# Install Brave Browser
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser -y
+
+# Making .config and Moving config files and background to Pictures
+cd $builddir
+mkdir -p /home/$username/.config
+mkdir -p /home/$username/.fonts
+mkdir -p /home/$username/Pictures
+mkdir -p /home/$username/Pictures/backgrounds
+cp -R dotconfig/* /home/$username/.config/
+cp bg.jpg /home/$username/Pictures/backgrounds/
+mv user-dirs.dirs /home/$username/.config
+chown -R $username:$username /home/$username
+
+# Installing Essential Programs 
+apt install feh kitty rofi picom thunar nitrogen x11-xserver-utils unzip wget pipewire wireplumber pavucontrol build-essential libx11-dev libxft-dev libxinerama-dev libx11-xcb-dev libxcb-res0-dev zoxide xdg-utils -y
+
+# Installing Other less important Programs
+apt install flameshot psmisc lxappearance papirus-icon-theme fonts-noto-color-emoji lightdm -y
+
+# Download Nordic Theme
+cd /usr/share/themes/
+git clone https://github.com/EliverLara/Nordic.git
+
+# Installing fonts
+cd $builddir 
+apt install fonts-font-awesome -y
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
+unzip FiraCode.zip -d /home/$username/.fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
+unzip Meslo.zip -d /home/$username/.fonts
+mv dotfonts/fontawesome/otfs/*.otf /home/$username/.fonts/
+chown $username:$username /home/$username/.fonts/*
+
+# Reloading Font
+fc-cache -vf
+# Removing zip Files
+rm ./FiraCode.zip ./Meslo.zip
+
+# Install Nordzy cursor
+git clone https://github.com/alvatip/Nordzy-cursors
+cd Nordzy-cursors
+./install.sh
+cd $builddir
+rm -rf Nordzy-cursors
+
+# Enable graphical login and change target from CLI to GUI
+systemctl set-default graphical.target
+
+# Enable wireplumber audio service
+sudo -u $username systemctl --user enable wireplumber.service
+
+# Beautiful bash
+git clone https://github.com/ChrisTitusTech/mybash
+cd mybash
+bash setup.sh
+cd $builddir
+
+# DWM Setup
+git clone https://github.com/ChrisTitusTech/dwm-titus
+cd dwm-titus
+make clean install
+cp dwm.desktop /usr/share/xsessions
+cd $builddir
+
+# Use apt
+bash scripts/useapt
