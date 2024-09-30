a1cfcd12486b2c8ceaaa26bc43e2e4125ea578a0
Tobirou-Senju <a9998964@gmail.com>
2024-09-30 09:09:17 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -65,6 +65,18 @@ cd Nordzy-cursors
 cd $builddir
 rm -rf Nordzy-cursors
 
+# Install the Web Browser
+ # Install brave-browser
+apt install curl
+
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+apt update
+
+apt install brave-browser
+
 # Enable graphical login and change target from CLI to GUI
 systemctl set-default graphical.target
 
